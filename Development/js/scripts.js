f1ebaf127f2eb788359a0ffbdba50cd4b0658632
Hassan Hussain <hassan@smart-itech.com>
2020-06-27 20:19:46 +0300

    class work

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,28 +1,44 @@
 // السؤال الأول
 // 1
+const name=("Hassan")
 
 // 2
+let age= 17;
 
 // 3 and 4
+console.log("I " +name+ " I am " +age)
 
 // 5 and 6
+console.log (age*5)
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
+const hobby=[ "Gaming","Football","Reading","Basketball" ];
 
 // 2
+hobby.push=(age);
 
 // 3
+hobby.pop()
 
 // 4
-
+console.log(hobby);
 // 5
+let web = {
+    development: "web Development",
+    bestlanguage: "html",
+    Ta: ("Nancy","Hussain", "Rahaf","Moudhi" )
+}
+    
+
 
 // 6
+console.log(web.development);
 
 // 7
+console.log(web.Ta);
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
@@ -32,3 +48,4 @@
 // 2
 
 // console results => console.png
+
